3dbe05cef20daf07808076ff497c111bb0d17d23
johnj974 <jjdug@hotmail.com>
2021-11-01 18:31:43 +0000

    some logic+basic style added

--- scripts/blackjack.js
+++ scripts/blackjack.js
@@ -1,1 +1,17 @@
-console.log("working");
+const dealBtn = document.querySelector("#deal-btn");
+const cardDisplay = document.querySelector("#card-display");
+const total = document.querySelector("#total");
+let totalCards = null;
+let acesHigh = false;
+
+dealBtn.addEventListener("click", () => {
+  cardDisplay.textContent = dealCard();
+});
+
+const dealCard = () => {
+  let suits = ["Hearts", "Diamonds", "Spades", "Clubs"];
+  let randomSuit = suits[Math.floor(Math.random() * 4)];
+  let randomCard = Math.floor(Math.random() * 13) + 1;
+  let returnText = `${randomCard} ${randomSuit}`;
+  return returnText;
+};
