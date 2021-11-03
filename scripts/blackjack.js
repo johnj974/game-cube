617b00c3701f2c9bf9afc3ba9cc880adcf28d1c7
johnj974 <jjdug@hotmail.com>
2021-11-03 19:47:54 +0000

    rough card layout for blackjack

--- scripts/blackjack.js
+++ scripts/blackjack.js
@@ -1,6 +1,8 @@
 const dealBtn = document.querySelector("#deal-btn");
 const cardDisplay = document.querySelector("#card-display");
 const total = document.querySelector("#total");
+let value = document.querySelector(".value");
+let suit = document.querySelector(".suit");
 let totalCards = null;
 let acesHigh = false;
 
@@ -12,6 +14,9 @@ const dealCard = () => {
   let suits = ["Hearts", "Diamonds", "Spades", "Clubs"];
   let randomSuit = suits[Math.floor(Math.random() * 4)];
   let randomCard = Math.floor(Math.random() * 13) + 1;
-  let returnText = `${randomCard} ${randomSuit}`;
+  value.textContent = randomCard;
+  suit.textContent = randomSuit;
+  //let returnText = `${value} ${suit}`;
+  // let returnText = `${randomCard} ${randomSuit}`;
   return returnText;
 };
