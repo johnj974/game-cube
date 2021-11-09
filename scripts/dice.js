8f4678b5b5471a3cfde047bcd98ed1e0b5f959c0
johnj974 <jjdug@hotmail.com>
2021-11-09 09:34:06 +0000

    dice unicodes added/better styling created

--- scripts/dice.js
+++ scripts/dice.js
@@ -1,10 +1,19 @@
+//
+const diceFaces = ["⚀", "⚁", "⚂", "⚃", "⚄", "⚅"];
+const diceDisplay = document.querySelector("#dice-display");
+let playerOneScore = document.querySelector(".player-one-score");
+let player1 = true;
+let playerOneTotal = 0;
+
 // action buttons
-const rollBtn = document.querySelector("#rollDice");
+const rollBtn = document.querySelector("#roll-dice");
 
 //functions
 let roll = () => {
   let diceRoll = Math.floor(Math.random() * 6) + 1;
-  console.log(diceRoll);
+  playerOneTotal += diceRoll;
+  playerOneScore.textContent = playerOneTotal;
+  diceDisplay.innerHTML = diceFaces[diceRoll - 1];
 };
 
 //events
