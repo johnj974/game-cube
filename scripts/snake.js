6b65376f64060f4a18974916344167e7c895a5c1
johnj974 <jjdug@hotmail.com>
2021-11-09 15:11:29 +0000

    some snake styling+logic

--- scripts/snake.js
+++ scripts/snake.js
@@ -1,8 +1,34 @@
-console.log("connected");
+let score = document.querySelector("#score");
+//board
+const grid = document.querySelector("#grid");
+const squares = [];
 
-// capture the grid/score/start button in variables
-// create a squares array
-// create a function that creates 100 divs within the grid
-// add a class to the individual divs
-// append the divs to the grid
-// push each newly created div into the squares array
+//snake
+let snake = [2, 1, 0];
+
+//buttons
+const startBtn = document.querySelector("#start-btn");
+
+//functions
+const createSquare = () => {
+  for (let i = 0; i < 1600; i++) {
+    let square = document.createElement("div");
+    square.classList.add("square");
+    grid.appendChild(square);
+    squares.push(square);
+  }
+};
+
+createSquare();
+
+snake.forEach((index) => {
+  squares[index].classList.add("snake");
+});
+
+const move = () => {
+  const removedSquare = snake.pop();
+  squares[removedSquare].classList.remove("square");
+  console.log(removedSquare);
+};
+
+move();
