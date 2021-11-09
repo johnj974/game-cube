dcfef2d904c2a5703e8afc8b460ce24e97fe368c
johnj974 <jjdug@hotmail.com>
2021-11-09 09:59:11 +0000

    snake files added

---
+++ scripts/snake.js
@@ -0,0 +1,8 @@
+console.log("connected");
+
+// capture the grid/score/start button in variables
+// create a squares array
+// create a function that creates 100 divs within the grid
+// add a class to the individual divs
+// append the divs to the grid
+// push each newly created div into the squares array
